8e38d936342b143ae7645afc76198ec7361d9b22
Alessandro Colantonio <alessandro.colantonio@hotmail.it>
2021-12-10 11:12:20 +0100

    Initial Commit

---
+++ MediaWorld App/MediaWorld_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaWorld_AppApp.swift
+//  MediaWorld App
+//
+//  Created by Alessandro Colantonio on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MediaWorld_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
